ad84d3b7d289cecbb9dada70d0106264fcf122e8
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-25 18:27:26 +0300

    [Connect] Saved endpoint "client.login"

--- server/client/login.js
+++ server/client/login.js
@@ -4,17 +4,22 @@
  * @param {Vars} vars
 */
 exports.submit = async (session, models, vars) => {
-    let output = await session.rest.cherwellapi.login({
-        grant_type_password: vars.config.rest.cherwellapi.custom.grant_type_password,
-        apikey: vars.config.rest.cherwellapi.custom.apikey,
-        username: models.login.username,
-        password: models.login.password
-    });
-    vars.session.access_token = output.body.access_token;
-    vars.session.refresh_token = output.body.refresh_token;
-    console.log('access_token: ' + vars.session.access_token);
-    let data = await session.rest.cherwellapi.getCustomerData({ access_token: vars.session.access_token });
-    vars.session.busObId = data.body[0].busObId;
-    console.log('busObId: ' + vars.session.busObId);
-    await session.screen('home');
+    try {
+        let output = await session.rest.cherwellapi.login({
+            grant_type_password: vars.config.rest.cherwellapi.custom.grant_type_password,
+            apikey: vars.config.rest.cherwellapi.custom.apikey,
+            username: models.login.username,
+            password: models.login.password
+        });
+        vars.session.access_token = output.body.access_token;
+        vars.session.refresh_token = output.body.refresh_token;
+        console.log('access_token: ' + vars.session.access_token);
+        let data = await session.rest.cherwellapi.getCustomerData({ access_token: vars.session.access_token });
+        vars.session.busObId = data.body[0].busObId;
+        console.log('busObId: ' + vars.session.busObId);
+        await session.screen('home');
+    } catch (e) {
+        console.log(e);
+        await session.screen('login');
+    }
 };
