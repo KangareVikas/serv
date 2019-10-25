14ab2bc99b9ad1c4d1314ed4d1490ea0ebcfbcf4
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-25 16:13:51 +0300

    [Connect] Saved endpoint "client.login"

--- server/client/login.js
+++ server/client/login.js
@@ -4,7 +4,7 @@
  * @param {Vars} vars
 */
 exports.submit = async (session, models, vars) => {
-    let output = await session.rest.cherwell.login({ apikey: vars.config.rest.cherwell.custom.apikey });
+    let output = await session.rest.cherwellapi.login({ apikey: vars.config.rest.cherwell.custom.apikey });
     let access_token = output.body.access_token;
     vars.session.access_token = access_token;
     console.log('access_token: ' + access_token);
