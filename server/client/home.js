a587318c4a3848bf124f17ccc650678cdcb471c5
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-28 16:29:16 +0200

    [Connect] Saved endpoint "client.home"

---
+++ server/client/home.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.doRequest = async (session, models, vars) => {
+};
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.doReport = async (session, models, vars) => {
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
 const session = require('smartux-connect');
 require('./client/login');
+require('./client/home');
 session.start();
