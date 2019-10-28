bb18eaa9e7fd6ab5acb2a2f56399230aa52fdaa4
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-28 15:50:42 +0200

    [Connect] Saved endpoint "client.login"

--- server/client/login.js
+++ server/client/login.js
@@ -14,11 +14,10 @@ exports.submit = async (session, models, vars) => {
         });
         vars.session.access_token = output.body.access_token;
         vars.session.refresh_token = output.body.refresh_token;
-        models.login.error = errorOutput.message;
         vars.session.apikey = models.login.apikey;
         console.log('access_token: ' + vars.session.access_token);
     } catch (e) {
-        console.log('Error msg: ' + e.message);
+        console.log('Error msg: ' + e.message.error);
         models.login.hasError = true;
         await session.screen('login');
         return;
