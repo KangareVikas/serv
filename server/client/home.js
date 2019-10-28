78b4fe21eba0970418eb471cd2b2eab7353267bc
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-28 17:35:43 +0200

    [Studio] Update source of file "server/client/home.js"

--- server/client/home.js
+++ server/client/home.js
@@ -5,15 +5,15 @@
 */
 exports.doRequest = async (session, models, vars) => {
     let data = await session.rest.cherwellapi.getBusinessObjectSummaryIncident({ access_token: vars.session.access_token });
-    vars.session.incidentBusObId = data.busObId;
-    console.log(data);
+    vars.session.incidentBusObId = data.body.busObId;
+    console.log(data.body);
     let requestData = await session.rest.cherwellapi.GetBusinessObjectTemplate({
         access_token: vars.session.access_token,
         busObId: vars.session.incidentBusObId,
         includeRequired: true,
         includeAll: false
     });
-    console.log(requestData);
+    console.log(requestData.body);
 };
 /**
  * @param {Session} session
