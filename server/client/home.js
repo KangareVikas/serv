b716295af71bf74636a55995ed6887ad627d0413
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-28 17:14:38 +0200

    [Connect] Saved endpoint "client.home"

--- server/client/home.js
+++ server/client/home.js
@@ -4,12 +4,8 @@
  * @param {Vars} vars
 */
 exports.doRequest = async (session, models, vars) => {
-    await session.rest.cherwellapi.getBusinessObjectSchema();
     await session.rest.cherwellapi.getBusinessObjectSummaryIncident({ access_token: vars.session.access_token });
-    await session.rest.cherwellapi.getCustomerRecId({
-        access_token: vars.session.access_token,
-        busObId: vars.session.busObId
-    });
+    console.log(data);
     await session.rest.cherwellapi.GetBusinessObjectTemplate({
         access_token: vars.session.access_token,
         busObId: vars.session.busObId,
