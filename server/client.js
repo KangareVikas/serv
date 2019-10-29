d0dfcf88f4cd429dbc9cad7beb871fa28279690e
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-29 13:12:06 +0200

    [Studio] Update source of file "server/client.js"

--- server/client.js
+++ server/client.js
@@ -4,5 +4,75 @@
  * @param {Vars} vars
 */
 exports.start = async (session, models, vars) => {
+    vars.session.selectionItems = {
+        'computer': {
+            label: 'Computer or Device',
+            suffix: 'because it',
+            list: [
+                'has a problem with power/charging',
+                'will not startup',
+                'has network connectivity issues',
+                'other'
+            ]
+        },
+        'desk': {
+            label: 'Desk Environment',
+            suffix: 'regarding my',
+            list: [
+                'monitor',
+                'keyboard and/or mouse',
+                'docking station',
+                'phone',
+                'other'
+            ]
+        },
+        'printer': {
+            label: 'Printer',
+            suffix: 'as it',
+            list: [
+                'won\'t setup Follow-Me printing',
+                'is unable to print',
+                'other'
+            ]
+        },
+        'applications': {
+            label: 'Software',
+            suffix: 'specifically',
+            list: [
+                'Microsoft products',
+                'ReserveNow',
+                'Jabber/Webex',
+                'other'
+            ]
+        },
+        'access': {
+            label: 'Access',
+            suffix: 'due to',
+            list: [
+                'unable to log-in',
+                'my account is disabled',
+                'issues with VPN/DUO',
+                'other'
+            ]
+        },
+        'building': {
+            label: 'Building / Facilities',
+            suffix: 'relating to',
+            list: [
+                'general building services',
+                'conference / room reservations',
+                'locker',
+                'other'
+            ]
+        }
+    };
+    vars.session.defaultCategory = "SUPPORT REQUEST > FOLLOW UP"
+    vars.session.buildingMap = {
+        'Sunnyvale Bldg 5': 'TECH CENTER WEST COAST',
+        'Sunnyvale Bldg 6': 'TECH CENTER WEST COAST',
+        'New York City HQ': 'TECH CENTER EAST COAST',
+        'Houston Main Campus': 'TECH CENTER CENTRAL REGION'
+    };
+    await session.transform.chp.start();
     await session.screen('login');
 };
