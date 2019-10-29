291ea4d8d902054219097c242d169fb4df4ffbfe
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-29 14:25:03 +0200

    [Studio] Update source of file "server/client/home.js"

--- server/client/home.js
+++ server/client/home.js
@@ -4,6 +4,7 @@
  * @param {Vars} vars
 */
 exports.doRequest = async (session, models, vars) => {
+    //TODO:
     let data = await session.rest.cherwellapi.getBusinessObjectSummaryIncident({ access_token: vars.session.access_token });
     vars.session.incidentBusObId = data.body[0].busObId;
     console.log(data.body);
@@ -23,23 +24,26 @@ exports.doRequest = async (session, models, vars) => {
  * @param {Vars} vars
 */
 exports.doReport = async (session, models, vars) => {
-    let output = await session.rest.cherwellapi.getBusinessObjectSchema({ custBusObId: vars.session.custBusObId, access_token: vars.session.access_token });
-    let parsed = output.body;
-    let firstNameFound = false;
-    let lastNameFound = false;
-    for (let i = 0; i < parsed.fieldDefinitions.length; i++) {
-        if (parsed.fieldDefinitions[i].displayName === 'First name') {
-            vars.session.firstNamefieldId = parsed.fieldDefinitions[i].fieldId;
-            firstNameFound = true;
-        }
-        if (parsed.fieldDefinitions[i].displayName === 'Last Name') {
-            vars.session.lastNamefieldId = parsed.fieldDefinitions[i].fieldId;
-            lastNameFound = true;
-        }
-        if (firstNameFound && lastNameFound) {
-            break;
+    if (!vars.session.firstNamefieldId || !vars.session.lastNamefieldId) {
+        console.log("Fetching firstNamefieldId and lastNamefieldId")
+        let output = await session.rest.cherwellapi.getBusinessObjectSchema({ custBusObId: vars.session.custBusObId, access_token: vars.session.access_token });
+        let parsed = output.body;
+        let firstNameFound = false;
+        let lastNameFound = false;
+        for (let i = 0; i < parsed.fieldDefinitions.length; i++) {
+            if (parsed.fieldDefinitions[i].displayName === 'First name') {
+                vars.session.firstNamefieldId = parsed.fieldDefinitions[i].fieldId;
+                firstNameFound = true;
+            }
+            if (parsed.fieldDefinitions[i].displayName === 'Last Name') {
+                vars.session.lastNamefieldId = parsed.fieldDefinitions[i].fieldId;
+                lastNameFound = true;
+            }
+            if (firstNameFound && lastNameFound) {
+                break;
+            }
         }
     }
-    console.log("FistNameFieldId: " + vars.session.firstNamefieldId)
+    console.log("firstNamefieldId: " + vars.session.firstNamefieldId)
     console.log("lastNamefieldId: " + vars.session.lastNamefieldId)
 };
