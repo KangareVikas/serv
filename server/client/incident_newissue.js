b3cc545596ee4dd4f8648dc57f5d2800c44c3a44
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-30 15:10:55 +0200

    [Connect] Saved endpoint "client.incident_newissue"

---
+++ server/client/incident_newissue.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.back = async (session, models, vars) => {
+    if (vars.session.selectedSubCatagoryLabel) {
+        await session.screen('incident_subcategories');
+    } else {
+        if (vars.session.selectedCatagoryLabel) {
+            await session.screen('incident_categories');
+        } else {
+            await session.screen('home');
+        }
+    }
+};
--- server/index.js
+++ server/index.js
@@ -3,4 +3,5 @@ require('./client/login');
 require('./client/home');
 require('./client/incident_categories');
 require('./client/incident_subcategories');
+require('./client/incident_newissue');
 session.start();
