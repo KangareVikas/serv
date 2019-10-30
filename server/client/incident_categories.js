a554245a0b7e7292f8954467047ddd65c03639a8
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-30 11:59:10 +0200

    [Connect] Saved endpoint "client.incident_categories"

---
+++ server/client/incident_categories.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.selectCategory = async (session, models, vars) => {
+};
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.search = async (session, models, vars) => {
+};
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.back = async (session, models, vars) => {
+};
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const session = require('smartux-connect');
 require('./client/login');
 require('./client/home');
+require('./client/incident_categories');
 session.start();
