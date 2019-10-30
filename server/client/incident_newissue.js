139d19cf1bcb8ab4a3ed1bd1fbcf6f72bbbdaaba
Yuriy Rudyi <yrudyi@softjourn.com>
2019-10-30 18:19:20 +0200

    [Connect] Saved endpoint "client.incident_newissue"

--- server/client/incident_newissue.js
+++ server/client/incident_newissue.js
@@ -8,28 +8,7 @@ exports.onload = async (session, models, vars) => {
     models.incident_newissue.forUser = 'Evan Employee';
     models.incident_newissue.email = 'evan.employee@acme.com';
     models.incident_newissue.phone = '6523455679';
-    models.incident_newissue.urgency = [
-        {
-            '$i': 0,
-            'label': '',
-            'value': '0'
-        },
-        {
-            '$i': 1,
-            'label': '1- High',
-            'value': '1'
-        },
-        {
-            '$i': 2,
-            'label': '2- Medium',
-            'value': '2'
-        },
-        {
-            '$i': 3,
-            'label': '3- Low',
-            'value': '3'
-        }
-    ];
+    models.incident_newissue.urgency = vars.session.urgencyMap;
 };
 /**
  * @param {Session} session
