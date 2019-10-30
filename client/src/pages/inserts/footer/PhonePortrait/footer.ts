fb4caa64d99dec88ca76c7dc629c52dce08c9b0d
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-30 14:54:03 +0200

    [Studio] Update mock data for "default", Modified screen "Footer"

--- client/src/pages/inserts/footer/PhonePortrait/footer.ts
+++ client/src/pages/inserts/footer/PhonePortrait/footer.ts
@@ -3,29 +3,32 @@ import { Screen } from 'app/screen';
 declare var window: any;
 
 @Component({
-  selector: 'screen-inserts-footer_phoneportrait',
-  templateUrl: 'footer.html'
+    selector: 'screen-inserts-footer_phoneportrait',
+    templateUrl: 'footer.html'
 })
 export class inserts_footer_PhonePortrait extends Screen {
-  data: any;
+    data: any;
 
-  ngOnInit(): void {
-    super.ngOnInit();
-    // Logic to run when the screen loads goes here.
-  }
+    ngOnInit(): void {
+        super.ngOnInit();
+        if (!this.data.footer) {
+            this.data.footer = {};
+        }
+        // Logic to run when the screen loads goes here.
+    }
 
-  ngOnDestroy(): void {
-    super.ngOnDestroy();
-    // Logic to run when the screen unloads goes here.
-  }
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+        // Logic to run when the screen unloads goes here.
+    }
 
-  onDataLoad(data: any): void {
-    // Logic to run when the screen's data is updated goes here.
-  }
-  onBackButton(): boolean {
-    //(Android) returns :
-    // true - handle the event in App Hooks
-    // false - stop the event propogation
-      return true;
-  }
+    onDataLoad(data: any): void {
+        // Logic to run when the screen's data is updated goes here.
+    }
+    onBackButton(): boolean {
+        //(Android) returns :
+        // true - handle the event in App Hooks
+        // false - stop the event propogation
+        return true;
+    }
 }
