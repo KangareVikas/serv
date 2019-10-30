ac1df463f305d494845c4a004b056c0486be1868
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-10-30 13:13:19 +0200

    [Connect] Saved endpoint "client.incident_categories"

--- server/client/incident_categories.js
+++ server/client/incident_categories.js
@@ -7,8 +7,10 @@ exports.selectCategory = async (session, models, vars) => {
     console.log(vars.params.title);
     if (vars.params.title === 'other') {
     } else {
+        let list = vars.session.selectionItems[vars.params.title].list;
+        let subcategories = [];
         models.incident_subcategories.category = undefined;
-        models.incident_subcategories.subcategories = undefined;
+        models.incident_subcategories.subcategories = subcategories;
         await session.screen('incident_subcategories');
     }
 };
