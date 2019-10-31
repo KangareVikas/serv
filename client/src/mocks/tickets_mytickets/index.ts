0622dbfff4c72798f18cbf3ffce8fc5f768f2984
Taras Romaniv <t.romaniv@gmail.com>
2019-10-31 18:43:01 +0200

    [Studio] Update mock actions for "tickets_mytickets"

--- client/src/mocks/tickets_mytickets/index.ts
+++ client/src/mocks/tickets_mytickets/index.ts
@@ -1,2 +1,17 @@
 export let mock_tickets_mytickets = {
+    search: async function search(params) {
+        await this.go("findticket");
+    },
+    footer_home: async function footer_home(params) {
+        await this.go("home");
+    },
+    tickets_view: async function tickets_view(params) {
+        await this.go("tickets_viewincident");
+    },
+    footer_myTickets: async function footer_myTickets(params) {
+        await this.go("tickets_mytickets");
+    },
+    footer_newIssue: async function footer_newIssue(params) {
+        await this.go("incident_newissue");
+    }
 };
