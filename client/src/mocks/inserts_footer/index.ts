c69bb052ca0b040e63270be9b8193c5778435d79
Taras Romaniv <t.romaniv@gmail.com>
2019-10-31 16:11:13 +0200

    [Studio] Update mock actions for "inserts_footer"

--- client/src/mocks/inserts_footer/index.ts
+++ client/src/mocks/inserts_footer/index.ts
@@ -1,2 +1,17 @@
 export let mock_inserts_footer = {
+    home: async function home(params) {
+        await this.go("home");
+    },
+    myTickets: async function myTickets(params) {
+        await this.go("tickets_mytickets");
+    },
+    call: async function call(params) {
+        alert("Call!");
+    },
+    newIssue: async function newIssue(params) {
+        await this.go("request_newrequest");
+    },
+    chat: async function chat(params) {
+        alert("Chat!");
+    }
 };
