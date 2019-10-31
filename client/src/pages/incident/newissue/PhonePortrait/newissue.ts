dabd785bd4a18c0d8949ade6a1aaa90760ed8c53
Oleksii <ohorbachevskyi@gmail.com>
2019-10-31 15:23:19 +0200

    [Studio] Modified screen "New Issue"

--- client/src/pages/incident/newissue/PhonePortrait/newissue.ts
+++ client/src/pages/incident/newissue/PhonePortrait/newissue.ts
@@ -8,50 +8,15 @@ declare var window: any;
 })
 export class incident_newissue_PhonePortrait extends Screen {
     data: any;
-    @ViewChild('top') top: ElementRef;
 
   ngOnInit(): void {
     super.ngOnInit();
     // Logic to run when the screen loads goes here.
   }
 
-  onKeyUp(event: any): void {
-      let value = event.value;
-      let regExp = /[^\d-]+/;
-
-      if (regExp.test(value)) {
-          event.value = value.slice(0, -1);
-      }
-
-    //   if (value.length === 4 || value.length === 9) {
-    //       console.log(this.data.callBackNumber);
-    //       console.log(value);
-    //       if (this.data.callBackNumber.length < value.length) {
-    //         event.value = value.slice(0, -1) + "-" + value[value.length - 1];
-    //       } else {
-    //         event.value = value.slice(0, -1)
-    //       }
-    //   } else {
-    //       if (value[value.length - 1] === "-") {
-    //           event.value = value.slice(0, -1);
-    //       } else {
-    //           if (regExp.test(value)) {
-    //               event.value = value.slice(0, -1);
-    //           }
-    //       }
-    //   }
-  }
-
   ngOnDestroy(): void {
     super.ngOnDestroy();
     // Logic to run when the screen unloads goes here.
-  }
-
-  onDataLoad(data: any): void {
-    // Logic to run when the screen's data is updated goes here.
-      if (this.top && this.data.errorMessage) {
-          this.top.nativeElement.scrollIntoView();
-      }
   }
 
   onBackButton(): boolean {
