6da26967ac29f99bad66e7969a4724873dbf3127
Yuriy Rudyi <yrudyi@softjourn.com>
2019-10-31 16:42:42 +0200

    [Studio] Update source of file "server/client/request_services.js"

--- server/client/request_services.js
+++ server/client/request_services.js
@@ -7,6 +7,7 @@ exports.onload = async (session, models, vars) => {
     let validValues = await session.rest.cherwellapi.getValidValues({ access_token: vars.session.access_token });
     let list = validValues.body.values;
     let subcategories = [];
+    list.map(item => { subcategories.push({ "title": item }) });
     models.request_services.services = subcategories;
     console.log(validValues.body);
 };
