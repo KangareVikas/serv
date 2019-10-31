effb15b332b0a82e50c150e9789c3a773cf0874a
Yuriy Rudyi <yrudyi@softjourn.com>
2019-10-31 15:11:19 +0200

    [Studio] Update source of file "server/client/finduser.js"

--- server/client/finduser.js
+++ server/client/finduser.js
@@ -4,6 +4,8 @@
  * @param {Vars} vars
 */
 exports.onload = async (session, models, vars) => {
+    models.finduser.users = [];
+    models.finduser.name = '';
 };
 /**
  * @param {Session} session
@@ -19,6 +21,24 @@ exports.search = async (session, models, vars) => {
         access_token: vars.session.access_token
     });
     console.log(result.body);
+    models.finduser.users = [];
+    let requiredFields = [
+        'LastName',
+        'FirstName',
+        'Phone',
+        'Manager'
+    ];
+    for (let i = 0; i < result.body.businessObjects.length; i++) {
+        let user = { customerRecId: result.body.businessObjects[i].busObRecId };
+        for (let j = 0; j < result.body.businessObjects[i].fields.length; j++) {
+            let fieldName = result.body.businessObjects[i].fields[j].name;
+            if (requiredFields.indexOf(fieldName) > -1) {
+                user[fieldName] = result.body.businessObjects[i].fields[j].value;
+            }
+        }
+        user.name = user['FirstName'] + ' ' + user['LastName']
+        models.finduser.users.push(user);
+    }
 };
 /**
  * @param {Session} session
@@ -34,4 +54,6 @@ exports.back = async (session, models, vars) => {
  * @param {Vars} vars
 */
 exports['users[].select'] = async (session, models, vars) => {
+    vars.session.customerRecId = vars.item.customerRecId;
+    await session.screen('incident_newissue');
 };
