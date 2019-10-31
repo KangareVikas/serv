a3759aa5cf67497c646294f5e850662d68b8b455
Yuriy Rudyi <yrudyi@softjourn.com>
2019-10-31 13:34:36 +0200

    [Studio] Update source of file "server/client/incident_newissue.js"

--- server/client/incident_newissue.js
+++ server/client/incident_newissue.js
@@ -16,6 +16,7 @@ exports.onload = async (session, models, vars) => {
     }
     models.incident_newissue.type = vars.session.selectionItemsMap;
 };
+
 /**
  * @param {Session} session
  * @param {Models} models
@@ -26,6 +27,24 @@ exports.cancel = async (session, models, vars) => {
     vars.session.selectedSubCatagoryLabel = null;
     await session.screen('home');
 };
+
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.back = async (session, models, vars) => {
+    if (vars.session.selectedSubCatagoryLabel) {
+        await session.screen('incident_subcategories');
+    } else {
+        if (vars.session.selectedCatagoryLabel) {
+            await session.screen('incident_categories');
+        } else {
+            await session.screen('home');
+        }
+    }
+}
+
 /**
  * @param {Session} session
  * @param {Models} models
