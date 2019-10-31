5b2a72011b567959bf25089362000b839cbd404e
Yuriy Rudyi <yrudyi@softjourn.com>
2019-10-31 14:59:21 +0200

    [Studio] Update source of file "server/client/finduser.js"

--- server/client/finduser.js
+++ server/client/finduser.js
@@ -19,6 +19,24 @@ exports.search = async (session, models, vars) => {
         access_token: vars.session.access_token
     });
     console.log(result.body);
+    models.finduser.users = [];
+    let requiredFields = [
+        'LastName',
+        'FirstName',
+        'Phone',
+        'Manager'
+    ];
+    for (let i = 0; i < result.body.businessObjects.length; i++) {
+        let user = { customerRecId: result.body.businessObjects[i].busObRecId };
+        for (let j = 0; j < result.body.businessObjects[i].fields.length; j++) {
+            let fieldName = result.body.businessObjects[i].fields[j].name;
+            if (requiredFields.indexOf(fieldName) > -1) {
+                user[fieldName] = result.body.businessObjects[i].fields[j].value;
+            }
+        }
+        user.name = user['FirstName'] + ' ' + user['LastName']
+        models.finduser.users.push(user);
+    }
 };
 /**
  * @param {Session} session
