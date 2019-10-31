ab7c0527130f611e7b42a49447595d6f7484552f
Yuriy Rudyi <yrudyi@softjourn.com>
2019-10-31 10:32:23 +0200

    [Connect] Saved endpoint "client.incident_newissue"

--- server/client/incident_newissue.js
+++ server/client/incident_newissue.js
@@ -4,15 +4,6 @@
  * @param {Vars} vars
 */
 exports.onload = async (session, models, vars) => {
-    if (vars.session.incidentBusObId) {
-    }
-    let requestData = await session.rest.cherwellapi.GetBusinessObjectTemplate({
-        access_token: vars.session.access_token,
-        busObId: vars.session.incidentBusObId,
-        includeRequired: true,
-        includeAll: false
-    });
-    console.log(requestData.body);
     models.incident_newissue.byUser = 'Evan Employee';
     models.incident_newissue.forUser = 'Evan Employee';
     models.incident_newissue.email = 'evan.employee@acme.com';
@@ -39,6 +30,61 @@ exports.cancel = async (session, models, vars) => {
  * @param {Vars} vars
 */
 exports.submit = async (session, models, vars) => {
+    let requestData = await session.rest.cherwellapi.GetBusinessObjectTemplate({
+        access_token: vars.session.access_token,
+        busObId: vars.session.incidentBusObId,
+        includeRequired: true,
+        includeAll: false
+    });
+    console.log(requestData.body);
+    let template = JSON.parse(requestData.body);
+    for (var i = 0; i < template.fields.length; i++) {
+        if (template.fields[i].name === 'Description') {
+            template.fields[i].value = 'Filing incident using REST API';
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'ShortDescription') {
+            template.fields[i].value = models.incident_newissue.type;
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'CustomerRecID') {
+            template.fields[i].value = '9451f6c8b5609372c4e86b440db32353b488fb4206';
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'Priority') {
+            template.fields[i].value = models.incident_newissue.urgency;
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'Source') {
+            template.fields[i].value = 'Portal';
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'Service') {
+            template.fields[i].value = 'Access Management';
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'Category') {
+            template.fields[i].value = 'Identity and Access Management';
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'Subcategory') {
+            template.fields[i].value = 'Submit Incident';
+            template.fields[i].dirty = true;
+            continue;
+        }
+        if (template.fields[i].name === 'OwnedByTeam') {
+            template.fields[i].value = 'Service Desk';
+            template.fields[i].dirty = true;
+            continue;
+        }
+    }
 };
 /**
  * @param {Session} session
