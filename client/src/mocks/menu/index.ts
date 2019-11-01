f49a0549a645b87b2c4d30ca87943885f0fdcdec
Taras Romaniv <t.romaniv@gmail.com>
2019-11-01 21:18:42 +0200

    [Studio] Update mock actions for "menu"

--- client/src/mocks/menu/index.ts
+++ client/src/mocks/menu/index.ts
@@ -2,4 +2,6 @@ export let mock_menu = { newIssue: async function newIssue(params) {
         await this.go("incident_newissue");
     }, myTickets: async function myTickets(params) {
         await this.go("tickets_mytickets");
+    }, chat: async function chat(params) {
+    }, call: async function call(params) {
     } };
