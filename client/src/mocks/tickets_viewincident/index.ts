a23abfb5d077a2b3ae2052991e521d97e6321f97
Taras Romaniv <t.romaniv@gmail.com>
2019-11-01 14:54:08 +0200

    [Studio] Update source of file "client/src/mocks/tickets_viewincident/default.json", Update source of file "client/src/mocks/tickets_viewincident/index.ts"

--- client/src/mocks/tickets_viewincident/index.ts
+++ client/src/mocks/tickets_viewincident/index.ts
@@ -1,2 +1,20 @@
 export let mock_tickets_viewincident = {
+    back: async function back(params) {
+        await this.go("tickets_mytickets");
+    },
+    footer_home: async function footer_home(params) {
+        await this.go("home");
+    },
+    footer_myTickets: async function footer_myTickets(params) {
+        await this.go("tickets_mytickets");
+    },
+    footer_chat: async function footer_chat(params) {
+    },
+    footer_call: async function footer_call(params) {
+    },
+    footer_newIssue: async function footer_newIssue(params) {
+        await this.go("incident_newissue");
+    },
+    attachments_download: async function attachments_download(params) {
+    }
 };
