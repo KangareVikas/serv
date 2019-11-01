a7f063444fc1e7a74890c950c4b4848c5b8df743
Yuriy Rudyi <yrudyi@softjourn.com>
2019-11-01 13:00:36 +0200

    [Connect] Saved endpoint "client.request_newrequest"

--- server/client/request_newrequest.js
+++ server/client/request_newrequest.js
@@ -4,6 +4,10 @@
  * @param {Vars} vars
 */
 exports.onload = async (session, models, vars) => {
+    if (!vars.session.problemBusObId) {
+        let data = await session.rest.cherwellapi.getBusinessObjectSummaryRequest({ access_token: vars.session.access_token });
+        vars.session.problemBusObId = data.body[0].busObId;
+    }
 };
 /**
  * @param {Session} session
