a8a250f7a71f672165ca215900f55e03f12db978
Oleksii <ohorbachevskyi@gmail.com>
2019-11-01 16:06:14 +0200

    [Connect] Saved endpoint "client.articles_findarticle"

--- server/client/articles_findarticle.js
+++ server/client/articles_findarticle.js
@@ -29,3 +29,11 @@ exports.onload = async (session, models, vars) => {
         models.articles_findarticle.articles.push(article);
     }
 };
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.back = async (session, models, vars) => {
+    await session.screen('home');
+};
