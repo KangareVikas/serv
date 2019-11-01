23d1e79d1038d11580dd177f19594f3674296d1d
Taras Romaniv <t.romaniv@gmail.com>
2019-11-01 21:17:06 +0200

    [Studio] Update screen model "menu", Update mock data for "default", Update mock actions for "menu", Modified screen "Side Menu"

--- client/src/mocks/menu/index.ts
+++ client/src/mocks/menu/index.ts
@@ -1,3 +1,5 @@
 export let mock_menu = { newIssue: async function newIssue(params) {
         await this.go("incident_newissue");
+    }, myTickets: async function myTickets(params) {
+        await this.go("tickets_mytickets");
     } };
