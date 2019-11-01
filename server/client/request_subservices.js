3bbac8fc87628f30d2f391d65bdd63bd8189db48
Yuriy Rudyi <yrudyi@softjourn.com>
2019-11-01 11:55:38 +0200

    [Connect] Saved endpoint "client.request_subservices"

--- server/client/request_subservices.js
+++ server/client/request_subservices.js
@@ -19,4 +19,15 @@ exports.back = async (session, models, vars) => {
  * @param {Vars} vars
 */
 exports['subservices[].select'] = async (session, models, vars) => {
+    let serviceTitle = vars.params.serviceTitle;
+    let categoryTitle = vars.params.categoryTitle;
+    let data = await session.rest.cherwellapi.getRequestSubCategoryValues({
+        incidentBusObId: vars.session.incidentBusObId,
+        subcategoryFieldId: vars.session.subcategoryFieldId,
+        serviceFieldId: vars.session.serviceFieldId,
+        access_token: vars.session.access_token,
+        serviceTitle: serviceTitle,
+        categoryFieldId: vars.session.categoryFieldId,
+        categoryTitle: categoryTitle
+    });
 };
