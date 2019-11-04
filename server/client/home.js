5829dbe9c95341281b80332adad895fd111c104a
Oleksii <ohorbachevskyi@gmail.com>
2019-11-04 11:45:59 +0200

    [Connect] Saved endpoint "client.home"

--- server/client/home.js
+++ server/client/home.js
@@ -38,7 +38,6 @@ exports.onload = async (session, models, vars) => {
     console.log('incidentBusObId: ' + vars.session.incidentBusObId);
     if (!vars.session.incidentFieldsIds) {
         console.log('Fetching fields IDs for Incedent');
-        await session.rest.cherwellapi.GetIncedentTemplate();
     }
     let openedTickets = await session.rest.cherwellapi.getAllincidents({
         access_token: vars.session.access_token,
