a441d7fb9bb5f32321f02edb77ca122d55479ec3
Oleksii <ohorbachevskyi@gmail.com>
2019-11-04 19:01:14 +0200

    [Connect] Saved endpoint "client.tickets_viewincident"

--- server/client/tickets_viewincident.js
+++ server/client/tickets_viewincident.js
@@ -3,6 +3,14 @@
  * @param {Models} models
  * @param {Vars} vars
 */
+exports.onload = async (session, models, vars) => {
+    models.tickets_viewincident.footer = { active: '' };
+};
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
 exports.back = async (session, models, vars) => {
     await session.screen('tickets_mytickets');
 };
