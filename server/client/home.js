7f4d63fc89fa456a47c2d2ddedf0ed14b24f24e1
Yuriy Rudyi <yrudyi@softjourn.com>
2019-11-04 14:22:25 +0200

    [Studio] Update source of file "server/client/home.js"

--- server/client/home.js
+++ server/client/home.js
@@ -116,7 +116,7 @@ exports.doReport = async (session, models, vars) => {
  * @param {Models} models
  * @param {Vars} vars
 */
-exports.footer['mytickets'] = async (session, models, vars) => {
+exports['footer.mytickets'] = async (session, models, vars) => {
     console.log('models: ', models);
     await session.screen('tickets_mytickets');
 };
