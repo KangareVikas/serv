8a7e05edf72a1400a5f47a0a779afc15c2269ab2
Oleksii <ohorbachevskyi@gmail.com>
2019-11-04 16:47:33 +0200

    [Connect] Saved endpoint "client.tickets_viewincident"

--- server/client/tickets_viewincident.js
+++ server/client/tickets_viewincident.js
@@ -3,12 +3,5 @@
  * @param {Models} models
  * @param {Vars} vars
 */
-exports.onload = async (session, models, vars) => {
-};
-/**
- * @param {Session} session
- * @param {Models} models
- * @param {Vars} vars
-*/
 exports.back = async (session, models, vars) => {
 };
