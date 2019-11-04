eca641f623b2ce617a8d87e5c3928b14ffa2f213
Oleksii <ohorbachevskyi@gmail.com>
2019-11-04 13:19:23 +0200

    [Connect] Saved endpoint "client.tickets_mytickets"

--- server/client/tickets_mytickets.js
+++ server/client/tickets_mytickets.js
@@ -4,4 +4,9 @@
  * @param {Vars} vars
 */
 exports.onload = async (session, models, vars) => {
+    let ticketsFilter = [{
+            'dirty': true,
+            'fieldId': vars.session.incidentFieldsIds['Status'].fieldId,
+            'value': 'In Progress'
+        }];
 };
