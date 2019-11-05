9cb4c0572f015da27f59759cbef0467c8ef42f15
Oleksii <ohorbachevskyi@gmail.com>
2019-11-05 10:42:06 +0200

    [Studio] Update source of file "server/client/articles_findarticle.js"

--- server/client/articles_findarticle.js
+++ server/client/articles_findarticle.js
@@ -37,3 +37,18 @@ exports.onload = async (session, models, vars) => {
 exports.back = async (session, models, vars) => {
     await session.screen('home');
 };
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports['footer.myTickets'] = async (session, models, vars) => {
+    await session.screen('tickets_mytickets');
+};/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports['footer.home'] = async (session, models, vars) => {
+    await session.screen('home');
+};
