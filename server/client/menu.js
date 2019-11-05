4be6d9cf286c42a79a52ce5ca3112e2214a4a425
Oleksii <ohorbachevskyi@gmail.com>
2019-11-05 14:01:48 +0200

    [Connect] Saved endpoint "client.menu"

--- server/client/menu.js
+++ server/client/menu.js
@@ -6,3 +6,10 @@
 exports.myTickets = async (session, models, vars) => {
     await session.screen('tickets_mytickets');
 };
+/**
+ * @param {Session} session
+ * @param {Models} models
+ * @param {Vars} vars
+*/
+exports.newIssue = async (session, models, vars) => {
+};
