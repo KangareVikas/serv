524b66d57d54e29cab1edc30336628be7323a31e
Vitalii Romaniv <vit.romaniv@gmail.com>
2019-11-05 13:09:54 +0200

    [Studio] Modified screen "My Tickets"

--- client/src/pages/tickets/mytickets/PhonePortrait/mytickets.ts
+++ client/src/pages/tickets/mytickets/PhonePortrait/mytickets.ts
@@ -3,29 +3,43 @@ import { Screen } from 'app/screen';
 declare var window: any;
 
 @Component({
-  selector: 'screen-tickets-mytickets_phoneportrait',
-  templateUrl: 'mytickets.html'
+    selector: 'screen-tickets-mytickets_phoneportrait',
+    templateUrl: 'mytickets.html'
 })
 export class tickets_mytickets_PhonePortrait extends Screen {
-  data: any;
+    data: any;
 
-  ngOnInit(): void {
-    super.ngOnInit();
-    // Logic to run when the screen loads goes here.
-  }
+    ngOnInit(): void {
+        super.ngOnInit();
+        // Logic to run when the screen loads goes here.
 
-  ngOnDestroy(): void {
-    super.ngOnDestroy();
-    // Logic to run when the screen unloads goes here.
-  }
+        this.data.sortOptions = {
+            "options": [
+                {
+                    "label": "Date",
+                    "value": "CreatedDateTime"
+                },
+                {
+                    "value": "IncidentID",
+                    "label": "Ticket ID"
+                }
+            ],
+            "selected": "CreatedDateTime"
+        };
+    }
 
-  onDataLoad(data: any): void {
-    // Logic to run when the screen's data is updated goes here.
-  }
-  onBackButton(): boolean {
-    //(Android) returns :
-    // true - handle the event in App Hooks
-    // false - stop the event propogation
-      return true;
-  }
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+        // Logic to run when the screen unloads goes here.
+    }
+
+    onDataLoad(data: any): void {
+        // Logic to run when the screen's data is updated goes here.
+    }
+    onBackButton(): boolean {
+        //(Android) returns :
+        // true - handle the event in App Hooks
+        // false - stop the event propogation
+        return true;
+    }
 }
