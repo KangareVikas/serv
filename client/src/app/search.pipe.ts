41b7a314adc99725b6cbcc28ce4926a983108a0c
Vitalii Romaniv <vit.romaniv@gmail.com>
2019-11-05 16:11:18 +0200

    [Studio] Update source of file "client/src/app/search.pipe.ts"

--- client/src/app/search.pipe.ts
+++ client/src/app/search.pipe.ts
@@ -2,12 +2,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'search' })
 export class SearchPipe implements PipeTransform {
-    transform(items: any[], terms: string, field: string): any[] {
+    transform(items: any[], terms: string, field: string, returnLength): any[] {
         if (!items) return [];
         if (!terms || !field) return items;
         terms = terms.toLowerCase();
-        return items.filter(item => {
+        const filtered = items.filter(item => {
             return item[field].toLowerCase().includes(terms);
         });
+        return returnLength ? filtered.length : filtered;
     }
 }
