f8fd67620baaad2530b415c8cd5cc8b013e0cc80
Vitalii Romaniv <vit.romaniv@gmail.com>
2019-11-06 13:41:48 +0200

    [Studio] Modified screen "Subservices"

--- client/src/pages/request/subservices/PhonePortrait/subservices.ts
+++ client/src/pages/request/subservices/PhonePortrait/subservices.ts
@@ -4,32 +4,43 @@ import { Screen } from 'app/screen';
 declare var window: any;
 
 @Component({
-  selector: 'screen-request-subservices_phoneportrait',
-  templateUrl: 'subservices.html'
+    selector: 'screen-request-subservices_phoneportrait',
+    templateUrl: 'subservices.html'
 })
 export class request_subservices_PhonePortrait extends Screen {
     // @ViewChild(Content) content: Content;
     @ViewChild('content') content;
-  data: any;
+    data: any;
 
-  ngOnInit(): void {
-    super.ngOnInit();
-    // Logic to run when the screen loads goes here.
-  }
+    ngOnInit(): void {
+        super.ngOnInit();
+        // Logic to run when the screen loads goes here.
+    }
 
-  ngOnDestroy(): void {
-    super.ngOnDestroy();
-    // Logic to run when the screen unloads goes here.
-  }
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+        // Logic to run when the screen unloads goes here.
+    }
 
-  onDataLoad(data: any): void {
-      this.content.resize();
-    // Logic to run when the screen's data is updated goes here.
-  }
-  onBackButton(): boolean {
-    //(Android) returns :
-    // true - handle the event in App Hooks
-    // false - stop the event propogation
-      return true;
-  }
+    onDataLoad(data: any): void {
+        this.content.resize();
+        // Logic to run when the screen's data is updated goes here.
+    }
+    onBackButton(): boolean {
+        //(Android) returns :
+        // true - handle the event in App Hooks
+        // false - stop the event propogation
+        return true;
+    }
+
+    goBack(): void {
+        if (this.data.selectedServices.length > 1) {
+            this.data.selectedServices.pop();
+            const servicesQuantity = this.data.selectedServices.length;
+            const title = this.data.selectedServices[servicesQuantity - 1].title;
+            this.method('goBack', { title: title });
+        } else {
+            this.action('back');
+        }
+    }
 }
