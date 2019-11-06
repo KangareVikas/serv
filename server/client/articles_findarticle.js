aed595b4774758c78cbceda6dfbc73dd01f92179
Vitalii Romaniv <vit.romaniv@gmail.com>
2019-11-06 18:42:00 +0200

    [Studio] Update source of file "server/client/articles_findarticle.js"

--- server/client/articles_findarticle.js
+++ server/client/articles_findarticle.js
@@ -48,7 +48,7 @@ exports.onload = async (session, models, vars) => {
  * @param {Vars} vars
 */
 exports.onunload = async (session, models, vars) => { 
-    models.articles_findarticle.searchKey = '';
+    delete models.articles_findarticle.searchKey;
 }
 /**
  * @param {Session} session
