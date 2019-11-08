1caa62d0e80402f8b2d1d72070b006361fd15709
Oleksii <ohorbachevskyi@gmail.com>
2019-11-08 13:15:45 +0200

    [Studio] Update source of file "client/src/app/screen.ts"

--- client/src/app/screen.ts
+++ client/src/app/screen.ts
@@ -6,6 +6,13 @@ config.keepAliveTimeout = 3 * 60 * 1000;
 export class Screen extends BaseScreen {
     protected global: GlobalData = DI.get<GlobalData>(GlobalData);
 
+    ngOnInit() {
+        if (window && window.screen && window.screen.orientation) {
+            window.screen.orientation.lock('portrait');
+        }
+        super.ngOnInit();
+    }
+
     logout(otherUserConnected?: boolean) {
         localStorage.removeItem('refresh_token');
         return super.logout(otherUserConnected);
