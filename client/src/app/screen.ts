35d75ccc54ce206af6305223a7e3f376227dd52a
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-11-12 08:46:43 -0800

    [Studio] Modified screen "Base Screen"

--- client/src/app/screen.ts
+++ client/src/app/screen.ts
@@ -6,10 +6,10 @@ config.keepAliveTimeout = 3 * 60 * 1000;
 export class Screen extends BaseScreen {
     protected global: GlobalData = DI.get<GlobalData>(GlobalData);
 
-    logout(otherUserConnected?: boolean) {
-        localStorage.removeItem('refresh_token');
-        return super.logout(otherUserConnected);
-    }
+    // logout(otherUserConnected?: boolean) {
+    //     localStorage.removeItem('refresh_token');
+    //     return super.logout(otherUserConnected);
+    // }
 
     ngOnInit() {
         if (window && window.screen && window.screen['orientation']) {
