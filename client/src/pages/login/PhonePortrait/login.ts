276b7896862291de3b363ee56b2bd6f56ed517d8
Vitalii Romaniv <vit.romaniv@gmail.com>
2019-11-13 21:24:31 +0200

    [Studio] Modified screen "Login"

--- client/src/pages/login/PhonePortrait/login.ts
+++ client/src/pages/login/PhonePortrait/login.ts
@@ -1,64 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Screen } from 'app/screen';
 
 declare var window: any;
 
 @Component({
-  selector: 'screen-login-phoneportrait',
-  templateUrl: 'login.html'
+    selector: 'screen-login-phoneportrait',
+    templateUrl: 'login.html'
 })
 export class login_PhonePortrait extends Screen {
-  data: any;
+    @ViewChild('content') content;
+    data: any;
 
-  ngOnInit(): void {
-    super.ngOnInit();
-    if (!this.data.username) {
-        this.data.username = 'evan.employee@acme.com'
+    ngOnInit(): void {
+        super.ngOnInit();
+        if (!this.data.username) {
+            this.data.username = 'evan.employee@acme.com'
+        }
+        // let refreshToken = localStorage.getItem('refresh_token');
+        // if (refreshToken) {
+        //     this.data.refresh_token = refreshToken;
+        // }
+        window.addEventListener('native.keyboardshow', this.keyboardShownHandler.bind(this));
+        window.addEventListener('native.keyboardhide', this.keyboardHiddenHandler.bind(this));
+        // Logic to run when the screen loads goes here.
     }
-    // let refreshToken = localStorage.getItem('refresh_token');
-    // if (refreshToken) {
-    //     this.data.refresh_token = refreshToken;
-    // }
-    window.addEventListener('native.keyboardshow', function () {
+
+    ngOnDestroy(): void {
+        super.ngOnDestroy();
+
+        window.removeEventListener('native.keyboardshow', this.keyboardShownHandler, true);
+        window.removeEventListener('native.keyboardhide', this.keyboardHiddenHandler, true);
+        // Logic to run when the screen unloads goes here.
+    }
+
+    onDataLoad(data: any): void {
+        //   if (data.errorMessage || data.invalid_refresh_token) {
+        //       localStorage.removeItem('refresh_token');
+        //       this.data.refresh_token = null;
+        //   } else {
+        //       let refreshToken = localStorage.getItem('refresh_token');
+        //       if (refreshToken) {
+        //           this.data.refresh_token = refreshToken;
+        //       }
+        //   }
+        // Logic to run when the screen's data is updated goes here.
+    }
+    onBackButton(): boolean {
+        //(Android) returns :
+        // true - handle the event in App Hooks
+        // false - stop the event propogation
+        return true;
+    }
+
+    callSupport() {
+        window.location.href = `tel:8778004381`
+    }
+
+    private keyboardShownHandler(event) {
+        this.content['_elementRef'].nativeElement.style.height = `calc(100% - ${event.keyboardHeight}px)`;
         document.activeElement.scrollIntoView(false);
-    });
-    // Logic to run when the screen loads goes here.
-  }
+    }
 
-  ngOnDestroy(): void {
-    super.ngOnDestroy();
-    // Logic to run when the screen unloads goes here.
-  }
+    private keyboardHiddenHandler() {
+        this.content['_elementRef'].nativeElement.style.height = '100%';
+    }
 
-  onDataLoad(data: any): void {
-    //   if (data.errorMessage || data.invalid_refresh_token) {
-    //       localStorage.removeItem('refresh_token');
-    //       this.data.refresh_token = null;
-    //   } else {
+    //   async ionViewDidEnter() {
+    //       await super.ionViewDidEnter();
     //       let refreshToken = localStorage.getItem('refresh_token');
     //       if (refreshToken) {
     //           this.data.refresh_token = refreshToken;
+    //           this.action('tokenSubmit');
     //       }
     //   }
-    // Logic to run when the screen's data is updated goes here.
-  }
-  onBackButton(): boolean {
-    //(Android) returns :
-    // true - handle the event in App Hooks
-    // false - stop the event propogation
-      return true;
-  }
-
-  callSupport() {
-      window.location.href = `tel:8778004381`
-  }
-
-//   async ionViewDidEnter() {
-//       await super.ionViewDidEnter();
-//       let refreshToken = localStorage.getItem('refresh_token');
-//       if (refreshToken) {
-//           this.data.refresh_token = refreshToken;
-//           this.action('tokenSubmit');
-//       }
-//   }
 }
