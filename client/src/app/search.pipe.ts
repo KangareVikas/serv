8cf9d45557e5053132b2ba88b0042c584ca041cd
Samir Mahendra <samir@powwowmobile.com>
2019-12-09 13:15:19 -0800

    [Studio] Update source of file "client/src/app/search.pipe.ts", Update source of file "client/src/pages/tickets/mytickets/PhonePortrait/mytickets.html"

--- client/src/app/search.pipe.ts
+++ client/src/app/search.pipe.ts
@@ -5,23 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
     pure: false
 })
 export class SearchPipe implements PipeTransform {
-    transform(items: any[], terms: string, fieldOrFields: any): any[] {
+    transform(items: any[], terms: string, fieldsCommaSeperatedList: string): any[] {
         if (!items) return [];
         if (!terms || !fieldOrFields) return items;
         terms = terms.toLowerCase();
+        let arrFields = fieldsCommaSeperatedList.split(',');
         return items.filter(item => {
-            let fieldToUse;
-            if (Array.isArray(fieldOrFields)) {
-                for (let field of fieldOrFields) {
-                    if (item.hasOwnProperty(field)) {
-                        fieldToUse = field;
-                        break;
-                    }
+            let fieldToUse = null;
+            for (let field of arrFields) {
+                if (item[field]) {
+                    fieldToUse = field;
+                    break;
                 }
-            } else {
-                fieldToUse = fieldOrFields;
             }
-            return item.hasOwnProperty[fieldToUse] && item[fieldToUse].toLowerCase().includes(terms);
+            return fieldToUse && item[fieldToUse] && item[fieldToUse].toLowerCase().includes(terms);
         });
     }
 }
