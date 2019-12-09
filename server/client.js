63856e98f9be68eabefce42b717a7c2c57fcdc0c
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-12-09 07:36:22 +0000

    [Studio] Update settings of application

--- server/client.js
+++ server/client.js
@@ -4,7 +4,6 @@
  * @param {Vars} vars
 */
 exports.start = async (session, models, vars) => {
-
     vars.session.newIssueCategories = {
         computer: {
             label: 'Computer or Device',
@@ -15,7 +14,11 @@ exports.start = async (session, models, vars) => {
                 { title: 'has network connectivity issues' },
                 { title: 'other' }
             ],
-            serviceClassification: ['Desktop Management', 'Computer', 'Submit Incident'],
+            serviceClassification: [
+                'Desktop Management',
+                'Computer',
+                'Submit Incident'
+            ],
             icon: 'assets/images/incident/categories/computer_or_device_icon.png',
             bg: 'assets/images/incident/categories/computer_or_device_bg_01.png'
         },
@@ -26,10 +29,21 @@ exports.start = async (session, models, vars) => {
                 { title: 'monitor' },
                 { title: 'keyboard and/or mouse' },
                 { title: 'docking station' },
-                { title: 'phone', serviceClassification: ['Telephony / Fax', 'Desktop Telephone', 'Submit Incident']},
+                {
+                    title: 'phone',
+                    serviceClassification: [
+                        'Telephony / Fax',
+                        'Desktop Telephone',
+                        'Submit Incident'
+                    ]
+                },
                 { title: 'other' }
             ],
-            serviceClassification: ['Desktop Management', 'Computer', 'Submit Incident'],
+            serviceClassification: [
+                'Desktop Management',
+                'Computer',
+                'Submit Incident'
+            ],
             icon: 'assets/images/incident/categories/desk environment_icon.png',
             bg: 'assets/images/incident/categories/desk_environment_bg.png'
         },
@@ -37,11 +51,22 @@ exports.start = async (session, models, vars) => {
             label: 'Printer',
             suffix: 'as it',
             list: [
-                { title: 'won\'t setup Follow-Me printing', serviceClassification: ['Printing', 'Network', 'Submit Incident'] },
+                {
+                    title: 'won\'t setup Follow-Me printing',
+                    serviceClassification: [
+                        'Printing',
+                        'Network',
+                        'Submit Incident'
+                    ]
+                },
                 { title: 'is unable to print' },
                 { title: 'other' }
             ],
-            serviceClassification: ['Printing', 'Desktop', 'Submit Incident'],
+            serviceClassification: [
+                'Printing',
+                'Desktop',
+                'Submit Incident'
+            ],
             icon: 'assets/images/incident/categories/printer_icon.png',
             bg: 'assets/images/incident/categories/printing_bg.png'
         },
@@ -49,13 +74,38 @@ exports.start = async (session, models, vars) => {
             label: 'Software',
             suffix: 'specifically',
             list: [
-                { title: 'Microsoft Office', serviceClassification: ['Enterprise Apps', 'Microsoft Office', 'Submit Incident'] },
-                { title: 'SalesForce', serviceClassification: ['Enterprise Apps', 'CRM/Sales Software', 'Submit Incident'] },
-                { title: 'Anti-Virus', serviceClassification: ['Desktop Management', 'Anti-Virus', 'Submit Incident'] },
+                {
+                    title: 'Microsoft Office',
+                    serviceClassification: [
+                        'Enterprise Apps',
+                        'Microsoft Office',
+                        'Submit Incident'
+                    ]
+                },
+                {
+                    title: 'SalesForce',
+                    serviceClassification: [
+                        'Enterprise Apps',
+                        'CRM/Sales Software',
+                        'Submit Incident'
+                    ]
+                },
+                {
+                    title: 'Anti-Virus',
+                    serviceClassification: [
+                        'Desktop Management',
+                        'Anti-Virus',
+                        'Submit Incident'
+                    ]
+                },
                 { title: 'Jabber/Webex' },
                 { title: 'other' }
             ],
-            serviceClassification: ['Desktop Management', 'Misc. Software', 'Submit Incident'],
+            serviceClassification: [
+                'Desktop Management',
+                'Misc. Software',
+                'Submit Incident'
+            ],
             icon: 'assets/images/incident/categories/software_icon.png',
             bg: 'assets/images/incident/categories/software_bg.png'
         },
@@ -77,84 +127,85 @@ exports.start = async (session, models, vars) => {
             bg: 'assets/images/incident/categories/other_technologies_bg.png'
         }
     };
-    vars.session.defaultServiceClassification = ['IT Service Desk', 'Report Outage or Error', 'Submit Incident'];
-    vars.session.urgencyDefaultValue = 'Medium'; //2;
-
+    vars.session.defaultServiceClassification = [
+        'IT Service Desk',
+        'Report Outage or Error',
+        'Submit Incident'
+    ];
+    vars.session.urgencyDefaultValue = 'Medium';
     vars.session.unknownServiceIconAndBg = {
-        icon: "assets/images/request/services/other_services_icon.png",
-        bg: "assets/images/request/services/other_services_bg.png"
+        icon: 'assets/images/request/services/other_services_icon.png',
+        bg: 'assets/images/request/services/other_services_bg.png'
     };
-
     vars.session.serviceToIconAndBgMapping = {
-        "Account Management": {
-            icon: "assets/images/request/services/security_icon.png",
-            bg: "assets/images/request/services/security_bg.png"
+        'Account Management': {
+            icon: 'assets/images/request/services/security_icon.png',
+            bg: 'assets/images/request/services/security_bg.png'
         },
-        "Conferencing / Presentation": {
-            icon: "assets/images/request/services/messaging_collaboration_icon.png",
-            bg: "assets/images/request/services/messaging_collaboration_bg.png"
+        'Conferencing / Presentation': {
+            icon: 'assets/images/request/services/messaging_collaboration_icon.png',
+            bg: 'assets/images/request/services/messaging_collaboration_bg.png'
         },
-        "Desktop Management": {
-            icon: "assets/images/request/services/workplace_icon.png",
-            bg: "assets/images/request/services/workplace_bg.png"
+        'Desktop Management': {
+            icon: 'assets/images/request/services/workplace_icon.png',
+            bg: 'assets/images/request/services/workplace_bg.png'
         },
-        "E-Mail / Calendaring": {
-            icon: "assets/images/request/services/access_icon.png",
-            bg: "assets/images/request/services/access_bg.png"
+        'E-Mail / Calendaring': {
+            icon: 'assets/images/request/services/access_icon.png',
+            bg: 'assets/images/request/services/access_bg.png'
         },
-        "Employee Support": {
-            icon: "assets/images/request/services/HR_icon.png",
-            bg: "assets/images/request/services/HR_bg.png"
+        'Employee Support': {
+            icon: 'assets/images/request/services/HR_icon.png',
+            bg: 'assets/images/request/services/HR_bg.png'
         },
-        "Enterprise Apps": {
-            icon: "assets/images/request/services/middleware.png",
-            bg: "assets/images/request/services/middleware_bg.png"
+        'Enterprise Apps': {
+            icon: 'assets/images/request/services/middleware.png',
+            bg: 'assets/images/request/services/middleware_bg.png'
         },
-        "IT Service Desk": {
-            icon: "assets/images/request/services/generic_services_icon.png",
-            bg: "assets/images/request/services/generic_services_bg.png"
+        'IT Service Desk': {
+            icon: 'assets/images/request/services/generic_services_icon.png',
+            bg: 'assets/images/request/services/generic_services_bg.png'
         },
-        "Network Services": {
-            icon: "assets/images/request/services/network_services_icon.png",
-            bg: "assets/images/request/services/network_service_bg.png"
+        'Network Services': {
+            icon: 'assets/images/request/services/network_services_icon.png',
+            bg: 'assets/images/request/services/network_service_bg.png'
         },
-        "Printing": {
-            icon: "assets/images/request/services/managed_print_service_icon.png",
-            bg: "assets/images/request/services/managed_print_service_bg.png"
+        Printing: {
+            icon: 'assets/images/request/services/managed_print_service_icon.png',
+            bg: 'assets/images/request/services/managed_print_service_bg.png'
         },
-        "Telephony / Fax": {
-            icon: "assets/images/request/services/telecom_service_icon.png",
-            bg: "assets/images/request/services/telecom_services_bg.png"
+        'Telephony / Fax': {
+            icon: 'assets/images/request/services/telecom_service_icon.png',
+            bg: 'assets/images/request/services/telecom_services_bg.png'
         },
-        "Web Services": {
-            icon: "assets/images/request/services/servers_management_icon.png",
-            bg: "assets/images/request/services/servers_management_bg.png"
+        'Web Services': {
+            icon: 'assets/images/request/services/servers_management_icon.png',
+            bg: 'assets/images/request/services/servers_management_bg.png'
         },
-        "unused_cloud": {
-            icon: "assets/images/request/services/cloud_services_icon.png",
-            bg: "assets/images/request/services/cloud_services.png"
+        unused_cloud: {
+            icon: 'assets/images/request/services/cloud_services_icon.png',
+            bg: 'assets/images/request/services/cloud_services.png'
         },
-        "unused_database": {
-            icon: "assets/images/request/services/database_services_icon.png",
-            bg: "assets/images/request/services/database_services_bg.png"
+        unused_database: {
+            icon: 'assets/images/request/services/database_services_icon.png',
+            bg: 'assets/images/request/services/database_services_bg.png'
         },
-        "unused_enablement": {
-            icon: "assets/images/request/services/enabling_IT_icon.png",
-            bg: "assets/images/request/services/enabling_IT_bg.png"
+        unused_enablement: {
+            icon: 'assets/images/request/services/enabling_IT_icon.png',
+            bg: 'assets/images/request/services/enabling_IT_bg.png'
         },
-        "unused_enterprise": {
-            icon: "assets/images/request/services/enterprise_services_icon.png",
-            bg: "assets/images/request/services/enterprise_services_bg.png"
+        unused_enterprise: {
+            icon: 'assets/images/request/services/enterprise_services_icon.png',
+            bg: 'assets/images/request/services/enterprise_services_bg.png'
         },
-        "unused_facilities": {
-            icon: "assets/images/request/services/facilities_icon.png",
-            bg: "assets/images/request/services/facilities_bg.png"
+        unused_facilities: {
+            icon: 'assets/images/request/services/facilities_icon.png',
+            bg: 'assets/images/request/services/facilities_bg.png'
         },
-        "unused_storage": {
-            icon: "assets/images/request/services/storage_services_icon.png",
-            bg: "assets/images/request/services/storage_services_bg.png"
-        },
+        unused_storage: {
+            icon: 'assets/images/request/services/storage_services_icon.png',
+            bg: 'assets/images/request/services/storage_services_bg.png'
+        }
     };
-
-    await session.screen('login');
+    await session.screen('initialize');
 };
