057f5bb568ae616b81831717fd48bede5641c531
Vitalii Romaniv <vit.romaniv@gmail.com>
2019-12-09 13:28:25 +0200

    [Studio] Screen "incident_subcategories" has been moved

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -4,8 +4,8 @@ import { login_PhonePortrait } from '../pages/login/PhonePortrait/login';
 import { home_PhonePortrait } from '../pages/home/PhonePortrait/home';
 import { inserts_homelogo_PhonePortrait } from '../pages/inserts/homelogo/PhonePortrait/homelogo';
 import { inserts_footer_PhonePortrait } from '../pages/inserts/footer/PhonePortrait/footer';
-import { incident_subcategories_PhonePortrait } from '../pages/incident/subcategories/PhonePortrait/subcategories';
 import { incident_categories_PhonePortrait } from '../pages/incident/categories/PhonePortrait/categories';
+import { incident_subcategories_PhonePortrait } from '../pages/incident/subcategories/PhonePortrait/subcategories';
 import { incident_newissue_PhonePortrait } from '../pages/incident/newissue/PhonePortrait/newissue';
 import { request_services_PhonePortrait } from '../pages/request/services/PhonePortrait/services';
 import { request_subservices_PhonePortrait } from '../pages/request/subservices/PhonePortrait/subservices';
@@ -28,8 +28,8 @@ export class Screens {
     home_PhonePortrait,
     inserts_homelogo_PhonePortrait,
     inserts_footer_PhonePortrait,
-    incident_subcategories_PhonePortrait,
     incident_categories_PhonePortrait,
+    incident_subcategories_PhonePortrait,
     incident_newissue_PhonePortrait,
     request_services_PhonePortrait,
     request_subservices_PhonePortrait,
@@ -59,11 +59,11 @@ export class Screens {
     'inserts_footer': {
       PhonePortrait: inserts_footer_PhonePortrait
     },
-    'incident_subcategories': {
-      PhonePortrait: incident_subcategories_PhonePortrait
-    },
     'incident_categories': {
       PhonePortrait: incident_categories_PhonePortrait
+    },
+    'incident_subcategories': {
+      PhonePortrait: incident_subcategories_PhonePortrait
     },
     'incident_newissue': {
       PhonePortrait: incident_newissue_PhonePortrait
