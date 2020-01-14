db18fca2ca9d27f61d13b1a34cb2b897b100c300
Vitalii Romaniv <vit.romaniv@gmail.com>
2020-01-14 18:06:27 +0200

    [Studio] Update source of file "server/client/inserts_footer.js"

--- server/client/inserts_footer.js
+++ server/client/inserts_footer.js
@@ -3,5 +3,6 @@
  * @param {Models} models
  * @param {Vars} vars
 */
-exports.onload = async (session, models, vars) => {
+exports.newIssue = async (session, models, vars) => {
+    vars.session.serviceClassification = vars.session.defaultServiceClassification;
 };
