49237a1cae80c4a3cafdbcde25ad500ac0256c0f
Vitalii Romaniv <vit.romaniv@gmail.com>
2020-01-14 18:09:06 +0200

    [Studio] Delete file "server/client/inserts_footer.js"

--- server/client/inserts_footer.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {Session} session
- * @param {Models} models
- * @param {Vars} vars
-*/
-exports.newIssue = async (session, models, vars) => {
-    vars.session.serviceClassification = vars.session.defaultServiceClassification;
-};
