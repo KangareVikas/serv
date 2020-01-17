3c3ea83eefac849e3247322af12b6ad7d074051d
Oleksii <ohorbachevskyi@gmail.com>
2020-01-17 15:38:03 +0200

    [Studio] Update source of file "server/client/articles_findarticle.js"

--- server/client/articles_findarticle.js
+++ server/client/articles_findarticle.js
@@ -109,6 +109,5 @@ exports['footer.newIssue'] = async (session, models, vars) => {
     vars.session.selectedCatagoryLabel = '';
     vars.session.selectedCatagorySuffix = '';
     vars.session.selectedSubCatagoryLabel = '';
-    vars.session.prevScreen = 'home';
     await session.screen('incident_newissue');
 };
