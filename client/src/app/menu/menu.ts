b8dcaec41287fd0bd4cb43b875acc29be3da29df
Samir Mahendra <samir@powwowmobile.com>
2020-01-31 03:01:39 -0800

    [Studio] Modified screen "Side Menu"

--- client/src/app/menu/menu.ts
+++ client/src/app/menu/menu.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { Screen } from 'app/screen';
-import { NavigationService } from 'smartux-client';
 
 @Component({
   selector: 'menu-component',
   templateUrl: 'menu.html'
 })
 export class MenuComponent extends Screen {
-    constructor(private navigationService: NavigationService) {
-        super();
-    }
 
     openAbout() {
         this.navigationService.go('about');
